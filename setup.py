e49cf677081839d0e61b4088ae6797c0759e1c95
Jean-Philippe Braun <eon@patapon.info>
2015-11-04 08:59:44 +0100

    Remove python 3.5 compat as keystoneclient doesn't support it yet

--- setup.py
+++ setup.py
@@ -39,8 +39,7 @@ setup(
         'Topic :: Software Development :: User Interfaces',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.4'
     ],
     keywords='contrail api cli',
     tests_require=test_requires,
