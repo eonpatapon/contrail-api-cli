f04b3cc81112f813c31a926a65cb9bec36acc05d
Jean-Philippe Braun <eon@patapon.info>
2015-12-01 16:43:12 +0100

    Fix match by fq_name

--- contrail_api_cli/commands.py
+++ contrail_api_cli/commands.py
@@ -82,7 +82,7 @@ def expand_paths(paths=None):
                 col = Collection(path.base, fetch=True)
             for r in col:
                 if (fnmatch(str(r.path), str(path)) or
-                        fnmatch(r.fq_name, str(path))):
+                        fnmatch(str(Path("/", r.type, r.fq_name)), str(path))):
                     result[r.path] = r
         elif ':' in path.name:
             try:
