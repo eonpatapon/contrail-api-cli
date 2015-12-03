c2f49ad8258c435847e132bb0952bc0f161a0572
Jean-Philippe Braun <eon@patapon.info>
2015-12-03 16:58:41 +0100

    Try to support better terms with only 8 colors

--- contrail_api_cli/style.py
+++ contrail_api_cli/style.py
@@ -1,10 +1,12 @@
+import curses
+
 from prompt_toolkit.styles import DefaultStyle
 
 from pygments.style import Style
 from pygments.token import Token
 
 
-class PromptStyle(Style):
+class Prompt256Style(Style):
     styles = DefaultStyle.styles.copy()
     styles.update({
         Token.Path: 'bold #009AC7',
@@ -16,9 +18,36 @@ class PromptStyle(Style):
         Token.Menu.Completions.Completion: 'bg:#74B3CC #204a87',
         Token.Menu.Completions.Completion.Current: 'bold bg:#274B7A #ffffff',
         Token.Menu.Completions.Meta: 'bg:#2C568C #eeeeee',
-        # Token.Menu.Completions.Meta.Current: 'bold bg:#74B3CC #204a87',
         Token.Menu.Completions.Meta.Current: 'bold bg:#274B7A #ffffff',
         Token.Menu.Completions.MultiColumnMeta: 'bg:#aaaaaa #000000',
         Token.Menu.Completions.ProgressBar: 'bg:#74B3CC',
         Token.Menu.Completions.ProgressButton: 'bg:#274B7A',
     })
+
+
+class Prompt8Style(Style):
+    styles = DefaultStyle.styles.copy()
+    styles.update({
+        Token.Path: 'bold #00ff00',
+        Token.Pound: 'bold',
+        Token.At: 'bold',
+        Token.Host: '#0000ff',
+        Token.Username: '#0000ff',
+
+        Token.Menu.Completions.Completion: 'bg:#ffffff #000000',
+        Token.Menu.Completions.Completion.Current: 'bold bg:#000000 #ffffff',
+        Token.Menu.Completions.Meta: 'bg:#ffffff #000000',
+        Token.Menu.Completions.Meta.Current: 'bold bg:#000000 #ffffff',
+        Token.Menu.Completions.MultiColumnMeta: 'bg:#000000 #ffffff',
+        Token.Menu.Completions.ProgressBar: 'bg:#ffffff',
+        Token.Menu.Completions.ProgressButton: 'bg:#000000',
+    })
+
+
+curses.setupterm()
+nb_colors = curses.tigetnum("colors")
+
+if nb_colors == 8:
+    PromptStyle = type('PromptStyle', (Prompt8Style,), {})
+else:
+    PromptStyle = type('PromptStyle', (Prompt256Style,), {})
