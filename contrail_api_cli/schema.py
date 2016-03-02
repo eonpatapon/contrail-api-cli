9d3315eb654892f55b6d9b8c0a333fe2d6df1ee3
Antoine Eiche <antoine.eiche@cloudwatt.com>
2016-03-02 18:42:04 +0100

    Initial commit of schema module
    This modules permits to parse Contrail schema xsd files in order to be
    able to statically know resource relationships.

---
+++ contrail_api_cli/schema.py
@@ -0,0 +1,87 @@
+"""This module creates schema data structure by parsing Contrail
+schema files.
+
+>>> schema = create_schema_from_xsd_directory("schema-directory")
+>>> schema.all_resources()
+>>> schema.resource('virtual-network').children
+
+"""
+
+from os import listdir
+from os.path import isfile, join
+
+import contrail_api_cli.idl_parser
+
+
+def _get_xsd_from_directory(pathname):
+    xsd_files = []
+    for f in listdir(pathname):
+        abspath = join(pathname, f)
+        if isfile(abspath) and abspath.endswith(".xsd"):
+            xsd_files.append(abspath)
+    return xsd_files
+
+
+def _parse_xsd_file(filename):
+    with open(filename) as f:
+        ifmap_statements = contrail_api_cli.idl_parser.IDLParser().Parse(f)
+        return ifmap_statements
+
+
+def create_schema_from_xsd_directory(directory):
+    """Create and fill the schema from a directory which contains xsd
+    files. It calls fill_schema_from_xsd_file for each xsd file
+    found.
+
+    """
+    schema = Schema()
+    for f in _get_xsd_from_directory(directory):
+        fill_schema_from_xsd_file(f, schema)
+    return schema
+
+
+def fill_schema_from_xsd_file(filename, schema):
+    """From an xsd file, it fills the schema by creating needed
+    Resource. The generateds idl_parser is used to parse ifmap
+    statements in the xsd file.
+
+    """
+    ifmap_statements = _parse_xsd_file(filename)
+
+    for v in ifmap_statements.values():
+        if (isinstance(v[0],
+                       contrail_api_cli.idl_parser.IDLParser.Link)):
+            src_name = v[1]
+            target_name = v[2]
+            src = schema._get_or_add_resource(src_name)
+            target = schema._get_or_add_resource(target_name)
+            if "has" in v[3]:
+                src.children.append(target_name)
+                target.parent = src_name
+            if "ref" in v[3]:
+                src.refs.append(target_name)
+                target.back_refs.append(src_name)
+
+
+class Schema(object):
+    def __init__(self):
+        self._schema = {}
+
+    def resource(self, resource_name):
+        return self._schema[resource_name]
+
+    def all_resources(self):
+        return self._schema.keys()
+
+    def _get_or_add_resource(self, resource_name):
+        if resource_name not in self._schema:
+            self._schema[resource_name] = Resource()
+        return self._schema[resource_name]
+
+
+class Resource(object):
+    def __init__(self):
+        self.children = []
+        self.parent = None
+        self.refs = []
+        self.back_refs = []
