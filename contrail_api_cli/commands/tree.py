7744baa736f7ab82306551612677377ea8f13e96
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2016-05-10 11:28:30 +0200

    Avoid loops in tree command

--- contrail_api_cli/commands/tree.py
+++ contrail_api_cli/commands/tree.py
@@ -5,6 +5,8 @@ from ..command import Command, Arg, expand_paths
 from ..resource import Resource
 from ..utils import format_tree, parallel_map
 
+NB_WORKERS = 100
+
 
 class Tree(Command):
     description = "Tree of resource references"
@@ -17,10 +19,11 @@ class Tree(Command):
                  help="Show tree of parents",
                  action="store_true", default=False)
 
-    def _create_tree(self, resource, reverse, parent):
+    def _create_tree(self, resource, reverse, parent, visited):
         tree = {}
         resource.fetch()
         tree['node'] = [str(self.current_path(resource)), str(resource.fq_name)]
+        visited.append(resource.uuid)
         if parent:
             childs = [resource.parent]
         elif reverse:
@@ -28,11 +31,16 @@ class Tree(Command):
         else:
             childs = resource.refs
         if childs:
-            tree['childs'] = parallel_map(self._create_tree, childs, args=(reverse, parent))
+            childs = [c for c in childs if c.uuid not in visited]
+            tree['childs'] = parallel_map(self._create_tree, childs,
+                                          args=(reverse, parent, visited),
+                                          workers=NB_WORKERS)
         return tree
 
     def __call__(self, paths=None, reverse=False, parent=False):
         resources = expand_paths(paths,
                                  predicate=lambda r: isinstance(r, Resource))
-        trees = parallel_map(self._create_tree, resources, args=(reverse, parent))
+        trees = parallel_map(self._create_tree, resources,
+                             args=(reverse, parent, []),
+                             workers=NB_WORKERS)
         return '\n'.join([format_tree(tree) for tree in trees])
