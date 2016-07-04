6b2f1d43866aa61b79f20fbcc7c3710358c22064
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2016-07-04 16:21:09 +0200

    Rework loop handling in tree command

--- contrail_api_cli/commands/tree.py
+++ contrail_api_cli/commands/tree.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from ..command import Command, Arg, Option, expand_paths
 from ..resource import Resource
-from ..utils import format_tree, parallel_map
+from ..utils import format_tree, parallel_map, Path
 from ..exceptions import ResourceMissing
 
 
@@ -18,28 +18,43 @@ class Tree(Command):
                     help="Show tree of parents",
                     action="store_true", default=False)
 
-    def _create_tree(self, resource, reverse, parent):
+    def _create_tree(self, resource, parent_path):
         tree = {}
         resource.fetch()
-        tree['node'] = [str(self.current_path(resource)), str(resource.fq_name)]
-        if parent:
+        tree['node'] = [str(self.current_path(resource)),
+                        str(resource.fq_name)]
+        tree['childs'] = []
+        if self.parent:
             try:
                 childs = [resource.parent]
             except ResourceMissing:
                 childs = []
-        elif reverse:
-            childs = resource.back_refs
+        elif self.reverse:
+            childs = list(resource.back_refs)
         else:
-            childs = resource.refs
+            childs = list(resource.refs)
+        # avoid parent -> child -> parent and parent -> parent loops
+        for idx, child in enumerate(childs[:]):
+            if (child.path == parent_path or
+                    child.path == resource.path):
+                node = {
+                    'node': [str(self.current_path(child)),
+                             str(child.fq_name)]
+                }
+                tree['childs'].append(node)
+                del childs[idx]
         if childs:
-            tree['childs'] = parallel_map(self._create_tree, childs, args=(reverse, parent))
+            tree['childs'] += parallel_map(self._create_tree, childs,
+                                           args=(resource.path,))
         return tree
 
     def __call__(self, paths=None, reverse=False, parent=False):
         resources = expand_paths(paths,
                                  predicate=lambda r: isinstance(r, Resource))
+        self.reverse = reverse
+        self.parent = parent
         trees = parallel_map(self._create_tree,
                              resources,
-                             args=(reverse, parent),
+                             args=(Path('/'),),
                              workers=50)
         return '\n'.join([format_tree(tree) for tree in trees])
