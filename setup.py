86ceb78e60b181ca84aa8c0e7520dbf58d91b9f5
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2016-11-02 10:03:23 +0100

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info[0] == 2:
 
 setup(
     name='contrail-api-cli',
-    version='0.1',
+    version='0.1.1',
     description="Simple CLI program to browse Contrail API server",
     long_description=open('README.md').read(),
     author="Jean-Philippe Braun",
