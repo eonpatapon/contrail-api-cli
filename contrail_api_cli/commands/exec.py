2ac420a27342cb50ccbdb0b65f2d9e72e013ee67
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2016-12-13 15:58:01 +0100

    Allow to pass args to exec script

--- contrail_api_cli/commands/exec.py
+++ contrail_api_cli/commands/exec.py
@@ -6,9 +6,27 @@ from ..command import Command, Arg
 
 
 class Exec(Command):
+    """Run python script inside the cli context.
+
+    The script content is executed using `exec` python
+    builtin.
+
+    All script arguments can be accessed from the args
+    variable inside the script::
+
+        contrail-api-cli exec test.py arg1 arg2
+
+    Inside the script args will be ['arg1', 'arg2'].
+
+    This method is used to write quick debugging scripts,
+    if you need a complete handling of arguments or options
+    consider to write a proper command.
+    """
     description = "Run a python script inside the cli"
     script = Arg(help="script path to run",
                  type=argparse.FileType('r'))
+    script_args = Arg(help="script args",
+                      nargs="*")
 
-    def __call__(self, script=None):
-        exec(script)
+    def __call__(self, script=None, script_args=None):
+        exec(script, {'args': script_args})
