824e4937fa5cf2f518b27a319bf6bbe22feb97f0
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2019-04-02 16:09:24 +0200

    tree: remove parent option
    Show parents as well as refs in normal mode
    Show back refs and children in reverse mode

--- contrail_api_cli/commands/tree.py
+++ contrail_api_cli/commands/tree.py
@@ -12,42 +12,39 @@ class Tree(Command):
 
     .. code-block:: bash
 
-        # tree of references
-        admin@localhost:/> tree logical-router/dd954810-d614-4892-9ec6-9a9595cc64ff
-        /logical-router/dd954810-d614-4892-9ec6-9a9595cc64ff                 default-domain:admin:router1
-        ├── /virtual-machine-interface/18b02f01-4300-427f-a646-0a44351034a6  default-domain:admin:18b02f01-4300-427f-a646-0a44351034a6
-        │   ├── /routing-instance/2f6907e9-20e7-415a-9969-bb5af375574d       default-domain:admin:net2:net2
-        │   │   ├── /route-target/721618d4-0861-4bda-8a33-bb116584d4bb       target:64512:8000005
-        │   │   ├── /route-target/d0e33aea-f63d-403b-a3e7-5bcef88e6053       target:64512:8000003
-        │   │   └── /route-target/ecd725e8-6523-428b-9811-00828926f91b       target:64512:8000002
-        │   ├── /virtual-network/49d00de8-4351-446f-b6ee-d16dec3de413        default-domain:admin:net2
-        │   │   └── /network-ipam/0edc36a1-c802-47be-b230-4b462d905b93       default-domain:default-project:default-network-ipam
-        │   └── /security-group/8282a986-b9fd-4be1-96bd-ab100bd2bb8e         default-domain:admin:default
-        ├── /virtual-machine-interface/6d9637e5-99ae-4d09-950e-50353b29411c  default-domain:admin:6d9637e5-99ae-4d09-950e-50353b29411c
-        │   ├── /routing-instance/5692de00-533a-4911-965b-dd9f6dbc6f55       default-domain:admin:net1:net1
-        │   │   ├── /route-target/79b5278c-a846-49f4-82ab-f1b8c05aff67       target:64512:8000001
-        │   │   └── /route-target/d0e33aea-f63d-403b-a3e7-5bcef88e6053       target:64512:8000003
-        │   ├── /virtual-network/5a9fbd42-a730-42f7-9947-be8a5d808b70        default-domain:admin:net1
-        │   │   └── /network-ipam/0edc36a1-c802-47be-b230-4b462d905b93       default-domain:default-project:default-network-ipam
-        │   └── /security-group/8282a986-b9fd-4be1-96bd-ab100bd2bb8e         default-domain:admin:default
-        └── /route-target/d0e33aea-f63d-403b-a3e7-5bcef88e6053               target:64512:8000003
+        # tree of back / childs references
+        localhost:/> tree -r service-instance/f8e191c5-83fa-47f1-a242-e8ad7cab46c0
+        service-instance/f8e191c5-83fa-47f1-a242-e8ad7cab46c0                     default-domain:project:a2fb6399-18ce-44ec-839d-d373221a6a8f
+        ├── loadbalancer/a2fb6399-18ce-44ec-839d-d373221a6a8f                     default-domain:project:lbv2
+        │   └── loadbalancer-listener/434b7b92-b68a-41f6-8321-8e7e2519d483        default-domain:project:lbv2_listener
+        │       └── loadbalancer-pool/82cce54d-b13a-427c-bac6-fb7e976bcf5f        default-domain:project:lbv2_pool
+        │           ├── loadbalancer-member/cc6e4c32-92c1-4668-879d-d6802f8ad3de  default-domain:project:lbv2_pool:cc6e4c32-92c1-4668-879d-d6802f8ad3de
+        │           └── loadbalancer-member/fb30633f-407b-461d-bf20-3ac9f0cbe504  default-domain:project:lbv2_pool:fb30633f-407b-461d-bf20-3ac9f0cbe504
+        ├── virtual-machine/68243a56-298d-4354-a077-b41b6bb55b7c                  default-domain__project__a2fb6399-18ce-44ec-839d-d373221a6a8f__1
+        │   └── virtual-machine-interface/4a0bf808-19d7-48e9-b076-54064bebaf2e    default-domain:project:default-domain__project__a2fb6399-18ce-44ec-839d-d373221a6a8f__1__right__1
+        │       └── instance-ip/44a9dc2a-446f-4d34-9148-4a2cc1f9a6f7              default-domain__project__a2fb6399-18ce-44ec-839d-d373221a6a8f-right
+        └── virtual-machine/114be9ed-ac45-4dc8-9f45-559470037cfd                  default-domain__project__a2fb6399-18ce-44ec-839d-d373221a6a8f__2
+            └── virtual-machine-interface/43afc4fe-8a84-47ba-96d9-73a8dd1d6b63    default-domain:project:default-domain__project__a2fb6399-18ce-44ec-839d-d373221a6a8f__2__right__1
+                └── instance-ip/44a9dc2a-446f-4d34-9148-4a2cc1f9a6f7              default-domain__project__a2fb6399-18ce-44ec-839d-d373221a6a8f-right
 
-        # tree of parents
-        admin@localhost:/> tree -p routing-instance/5692de00-533a-4911-965b-dd9f6dbc6f55
-        /routing-instance/5692de00-533a-4911-965b-dd9f6dbc6f55     default-domain:admin:net1:net1
-        └── /virtual-network/5a9fbd42-a730-42f7-9947-be8a5d808b70  default-domain:admin:net1
-            └── /project/d0afbb0b-dd83-4a33-a673-9cb2b244e804      default-domain:admin
-                └── /domain/cbc6051f-fd47-4a26-82ee-cb3482926e17   default-domain
+
+        # tree of parents / references
+        admin@localhost:/> tree routing-instance/f792b52d-ff69-487e-b2a5-c13060e3ce77
+        routing-instance/f792b52d-ff69-487e-b2a5-c13060e3ce77        default-domain:project:test1:test1
+        ├── route-target/7454c39a-b3c7-4d42-b766-bd276710d0b1        target:64518:8000133
+        └── virtual-network/ce88182e-4e6e-4c61-9df1-7ffb24543578     default-domain:project:test1
+            ├── network-ipam/d6bdafea-58d2-4240-8c65-2acf837d6750    default-domain:default-project:default-network-ipam
+            │   └── project/acb58362-0272-4d27-97a1-da4ac1e2c5e3     default-domain:default-project
+            │       └── domain/ff62f8f7-cccd-4a30-ba32-2ee3764fac79  default-domain
+            └── project/0ed483e0-83ef-4f70-8250-1fcfa5d98c0e         default-domain:project
+                └── domain/ff62f8f7-cccd-4a30-ba32-2ee3764fac79      default-domain
     """
     description = "Tree of resource references"
     paths = Arg(nargs="*", help="Resource path(s)",
                 metavar='path', complete='resources::path')
     reverse = Option('-r',
-                     help="Show tree of back references",
+                     help="Show tree of back references / children",
                      action="store_true", default=False)
-    parent = Option('-p',
-                    help="Show tree of parents",
-                    action="store_true", default=False)
 
     def _create_tree(self, resource, parent_path):
         tree = {}
@@ -55,15 +52,14 @@ class Tree(Command):
         tree['node'] = [str(self.current_path(resource)),
                         str(resource.fq_name)]
         tree['childs'] = []
-        if self.parent:
-            try:
-                childs = [resource.parent]
-            except ResourceMissing:
-                childs = []
-        elif self.reverse:
-            childs = list(resource.back_refs)
+        if self.reverse:
+            childs = list(resource.back_refs) + list(resource.children)
         else:
-            childs = list(resource.refs)
+            try:
+                parents = [resource.parent]
+            except ResourceMissing:
+                parents = []
+            childs = list(resource.refs) + parents
         # avoid parent -> child -> parent and parent -> parent loops
         for idx, child in enumerate(childs[:]):
             if (child.path == parent_path or
@@ -79,11 +75,10 @@ class Tree(Command):
                                            args=(resource.path,))
         return tree
 
-    def __call__(self, paths=None, reverse=False, parent=False):
+    def __call__(self, paths=None, reverse=False):
         resources = expand_paths(paths,
                                  predicate=lambda r: isinstance(r, Resource))
         self.reverse = reverse
-        self.parent = parent
         trees = parallel_map(self._create_tree,
                              resources,
                              args=(Path('/'),),
