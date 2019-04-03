afedddea1d887f87fd6061de585e07e5791e7629
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2019-04-03 10:41:50 +0200

    tree: show back_refs/children in defaut mode
    Add cache to not retrieve the same resource multiple times

--- contrail_api_cli/commands/tree.py
+++ contrail_api_cli/commands/tree.py
@@ -13,7 +13,7 @@ class Tree(Command):
     .. code-block:: bash
 
         # tree of back / childs references
-        localhost:/> tree -r service-instance/f8e191c5-83fa-47f1-a242-e8ad7cab46c0
+        localhost:/> tree service-instance/f8e191c5-83fa-47f1-a242-e8ad7cab46c0
         service-instance/f8e191c5-83fa-47f1-a242-e8ad7cab46c0                     default-domain:project:a2fb6399-18ce-44ec-839d-d373221a6a8f
         ├── loadbalancer/a2fb6399-18ce-44ec-839d-d373221a6a8f                     default-domain:project:lbv2
         │   └── loadbalancer-listener/434b7b92-b68a-41f6-8321-8e7e2519d483        default-domain:project:lbv2_listener
@@ -27,9 +27,8 @@ class Tree(Command):
             └── virtual-machine-interface/43afc4fe-8a84-47ba-96d9-73a8dd1d6b63    default-domain:project:default-domain__project__a2fb6399-18ce-44ec-839d-d373221a6a8f__2__right__1
                 └── instance-ip/44a9dc2a-446f-4d34-9148-4a2cc1f9a6f7              default-domain__project__a2fb6399-18ce-44ec-839d-d373221a6a8f-right
 
-
         # tree of parents / references
-        admin@localhost:/> tree routing-instance/f792b52d-ff69-487e-b2a5-c13060e3ce77
+        admin@localhost:/> tree -r routing-instance/f792b52d-ff69-487e-b2a5-c13060e3ce77
         routing-instance/f792b52d-ff69-487e-b2a5-c13060e3ce77        default-domain:project:test1:test1
         ├── route-target/7454c39a-b3c7-4d42-b766-bd276710d0b1        target:64518:8000133
         └── virtual-network/ce88182e-4e6e-4c61-9df1-7ffb24543578     default-domain:project:test1
@@ -43,16 +42,20 @@ class Tree(Command):
     paths = Arg(nargs="*", help="Resource path(s)",
                 metavar='path', complete='resources::path')
     reverse = Option('-r',
-                     help="Show tree of back references / children",
+                     help="Show tree of refs / parents",
                      action="store_true", default=False)
 
     def _create_tree(self, resource, parent_path):
-        tree = {}
-        resource.fetch()
-        tree['node'] = [str(self.current_path(resource)),
-                        str(resource.fq_name)]
-        tree['childs'] = []
-        if self.reverse:
+        if resource.uuid not in self._cache:
+            self._cache[resource.uuid] = resource.fetch()
+        else:
+            resource = self._cache[resource.uuid]
+        tree = {
+            'node': [str(self.current_path(resource)),
+                     str(resource.fq_name)],
+            'childs': []
+        }
+        if not self.reverse:
             childs = list(resource.back_refs) + list(resource.children)
         else:
             try:
@@ -78,6 +81,7 @@ class Tree(Command):
     def __call__(self, paths=None, reverse=False):
         resources = expand_paths(paths,
                                  predicate=lambda r: isinstance(r, Resource))
+        self._cache = {}
         self.reverse = reverse
         trees = parallel_map(self._create_tree,
                              resources,
